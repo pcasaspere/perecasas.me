5ad7024260ff06d56f3f002bcf881fe160a74421
Pere Casas <pcasas@atendata.net>
2024-01-16 22:53:55 +0100

    fix ui

--- components/content/CodeMockup.vue
+++ components/content/CodeMockup.vue
@@ -1,5 +1,8 @@
+<script setup>
+
+</script>
 <template>
   <div class="mockup-code">
-    <ContentSlot :use="$slots.default" unwrap="p" />
+    <pre data-prefix="$"><ContentSlot :use="$slots.default" unwrap="p" /></pre>
   </div>
 </template>
--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -17,7 +17,9 @@ useSeoMeta({
 </script>
 <template>
   <section class="flex flex-col">
+    
     <BlogArticle :document="document" />
+    
     <NuxtLink class="text-primary" :to="{ name: 'index' }">
       <button class="btn btn-ghost">← Volver al inicio</button>
     </NuxtLink>
