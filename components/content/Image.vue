d8ea076a9bf550078269c5864b8b952eadf14eba
Pere Casas <pcasas.pere@gmail.com>
2024-01-17 08:15:52 +0100

    add auto alt attirbute && change blog path

--- components/content/Image.vue
+++ components/content/Image.vue
@@ -1,13 +1,19 @@
 <script setup>
 // https://image.nuxt.com/usage/nuxt-img
-defineProps(['alt', 'src'])
+const props = defineProps(["alt", "src"]);
+
+const altData = computed(
+  () => props.alt || props.src.replace(/[\(\)\/\-_\.]/g, " ").trim()
+);
 </script>
 
 <template>
+  {{ altData }}
   <NuxtImg
     class="w-full md:max-w-3xl hover:scale-110 hover:md:scale-110 transition-transform duration-500 rounded-lg hover:rounded-2xl shadow-lg mx-auto"
-    :alt="alt"
+    :alt="altData"
     :src="src"
+    :title="alt"
     loading="lazy"
     format="webp"
   />
