44afc912af5dc22ea24dc1e4298ff92f46ed5414
Pere Casas <pcasas.pere@gmail.com>
2024-01-17 14:11:08 +0100

    fix image

--- components/content/Image.vue
+++ components/content/Image.vue
@@ -8,7 +8,6 @@ const altData = computed(
 </script>
 
 <template>
-  {{ altData }}
   <NuxtImg
     class="w-full md:max-w-3xl hover:scale-110 hover:md:scale-110 transition-transform duration-500 rounded-lg hover:rounded-2xl shadow-lg mx-auto"
     :alt="altData"
