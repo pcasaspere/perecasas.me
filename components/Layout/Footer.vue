807bc2757500008b2d03129e4c1944fcb6982f84
Pere Casas <pcasas.pere@gmail.com>
2024-09-16 19:47:22 +0200

    Remove X  (sniff... sniff...)

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -3,41 +3,38 @@
   <footer>
     <div class="mt-16 flex flex-col items-center">
       <div class="mb-3 flex space-x-4">
-        <NuxtLink
-          class="text-sm text-gray-500 transition hover:text-gray-600"
-          :to="{ name: 'meeting' }"
-          ><span class="sr-only">contact</span
-          ><IconsEmail class="h-6 w-6" /></NuxtLink
-        ><NuxtLink
-          class="text-sm text-gray-500 transition hover:text-gray-600"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/pcasaspere"
-          ><span class="sr-only">github</span><IconsGithub class="h-6 w-6"
-        /></NuxtLink>
+        <NuxtLink class="text-sm text-gray-500 transition hover:text-gray-600" :to="{ name: 'meeting' }"><span
+            class="sr-only">contact</span>
+          <IconsEmail class="h-6 w-6" />
+        </NuxtLink>
+        <NuxtLink class="text-sm text-gray-500 transition hover:text-gray-600" target="_blank" rel="noopener noreferrer"
+          href="https://github.com/pcasaspere"><span class="sr-only">github</span>
+          <IconsGithub class="h-6 w-6" />
+        </NuxtLink>
 
-        <NuxtLink
-          class="text-sm text-gray-500 transition hover:text-gray-600"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.linkedin.com/in/pere-casas/"
-          ><span class="sr-only">linkedin</span
-          ><IconsLinkedin class="h-6 w-6" /></NuxtLink
-        ><NuxtLink
-          class="text-sm text-gray-500 transition hover:text-gray-600"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://twitter.com/CasasPerep"
-          ><span class="sr-only">twitter</span><IconsTwitter class="w-6 h-6"
-        /></NuxtLink>
+        <NuxtLink class="text-sm text-gray-500 transition hover:text-gray-600" target="_blank" rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/pere-casas/"><span class="sr-only">linkedin</span>
+          <IconsLinkedin class="h-6 w-6" />
+        </NuxtLink>
+        <!--
+          <NuxtLink
+            class="text-sm text-gray-500 transition hover:text-gray-600"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/CasasPerep"
+            ><span class="sr-only">twitter</span><IconsTwitter class="w-6 h-6"
+          /></NuxtLink> 
+        -->
+        <NuxtLink class="text-sm text-gray-500 transition hover:text-gray-600" target="_blank" rel="me"
+          href="https://mastodont.cat/@pcasas"><span class="sr-only">mastodon</span>
+          <IconsMastodon class="h-6 w-6" />
+        </NuxtLink>
       </div>
 
       <div class="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
         <NuxtLink :to="{ name: 'legal-aviso-legal' }">Aviso legal</NuxtLink>
         <span>|</span>
-        <NuxtLink :to="{ name: 'legal-politica-privacidad' }"
-          >Política de privacidad</NuxtLink
-        >
+        <NuxtLink :to="{ name: 'legal-politica-privacidad' }">Política de privacidad</NuxtLink>
         <span>|</span>
         <NuxtLink :to="{ name: 'legal-politica-cookies' }">
           Política de cookies
